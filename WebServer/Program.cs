b6051f9962a88573b1dc946a791e56e37118ed3c
Jacob Ulrich <julrich@gmail.com>
2017-11-27 10:21:54 -0800

    Initial Commit

---
+++ WebServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+
+    public class WebServer
+    {
+        private readonly HttpListener _listener = new HttpListener();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
